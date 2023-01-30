db259793d7c566134223697b38ffdf5a869dea39
OnurHG <onurhangumus@gmail.com>
2023-01-30 09:56:14 +0300

    -Tutorial started

---
+++ Assets/Scripts/Managers/TutorialManager.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using Commands;
+using Controllers;
+using Data.UnityObject;
+using Data.ValueObject;
+using Enums;
+using Signals;
+using UnityEngine;
+
+namespace Managers
+{
+    public class TutorialManager : MonoBehaviour
+    {
+        #region Self Variables
+
+        #region Public Variables
+
+        #endregion
+
+        #region Serialized Variables
+        [SerializeField] private Transform pointer;
+        #endregion
+
+        #region Private Variables
+        private PlayerData _data;
+        #endregion
+
+        #endregion
+
+        private void Awake()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            _data = GetData();
+        }
+        public PlayerData GetData() => Resources.Load<CD_Player>("Data/CD_Player").Data;
+
+        #region Event Subscription
+
+        private void OnEnable()
+        {
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            CoreGameSignals.Instance.onPlay += OnPlay;
+            CoreGameSignals.Instance.onRestartLevel += OnResetLevel;
+        }
+
+        private void UnsubscribeEvents()
+        {
+            CoreGameSignals.Instance.onPlay -= OnPlay;
+            CoreGameSignals.Instance.onRestartLevel -= OnResetLevel;
+        }
+
+
+        private void OnDisable()
+        {
+            UnsubscribeEvents();
+        }
+
+        #endregion
+        private void OnPlay()
+        {
+            if (true)
+            {
+
+            }
+        }
+        private void OnResetLevel()
+        {
+
+        }
+    }
+}
